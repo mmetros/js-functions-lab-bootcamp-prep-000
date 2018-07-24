a266a3abd8cc158fd6a2f3b87489e9a26f0fb6b8
mmetros@wesleyan.edu <mmetros@wesleyan.edu>
2018-07-24 13:17:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  
+}
